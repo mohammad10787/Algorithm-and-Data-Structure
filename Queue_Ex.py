0e2222407c7c213cc261eb1a4369d4e8e8dd5b86
Mohammad Afshari <mohammad.afshari2@mail.mcgill.ca>
2024-04-19 00:07:19 -0700

    Update Queue_Ex.py

--- Queue_Ex.py
+++ Queue_Ex.py
@@ -1,42 +1,85 @@
+# from collections import deque
+# import time
+# import threading
+#
+# class Queue:
+#
+#     def __init__(self):
+#         self.buffer = deque()
+#
+#     def enqueue(self, orders):
+#         for order in orders:
+#             print("Placing order for:", order)
+#             self.buffer.appendleft(order)
+#             time.sleep(0.5)
+#
+#     def dequeue(self):
+#         time.sleep(1)
+#         while self.is_empty():
+#             order = self.buffer.pop()
+#             print("Now serving:", order)
+#             time.sleep(2)
+#
+#
+#     def is_empty(self):
+#         return len(self.buffer)
+#
+#     def size(self):
+#         return len(self.buffer)
+#
+# if __name__ == "__main__":
+#     orders = ['pizza', 'samosa', 'pasta', 'biryani', 'burger']
+#
+#     pq = Queue()
+#
+#     t1 = threading.Thread(target=pq.enqueue, args=(orders,))
+#     t2 = threading.Thread(target=pq.dequeue)
+#
+#     t1.start()
+#     t2.start()
+#
+#     t1.join()
+#     t2.join()
+
 from collections import deque
-import time
-import threading
 
 class Queue:
-
     def __init__(self):
         self.buffer = deque()
 
-    def enqueue(self, orders):
-        for order in orders:
-            print("Placing order for:", order)
-            self.buffer.appendleft(order)
-            time.sleep(0.5)
+    def enqueue(self, val):
+        self.buffer.appendleft(val)
 
     def dequeue(self):
-        time.sleep(1)
-        while self.is_empty():
-            order = self.buffer.pop()
-            print("Now serving:", order)
-            time.sleep(2)
+        if len(self.buffer)==0:
+            print("Queue is empty")
+            return
 
+        return self.buffer.pop()
 
     def is_empty(self):
-        return len(self.buffer)
+        return len(self.buffer) == 0
 
     def size(self):
         return len(self.buffer)
 
-if __name__ == "__main__":
-    orders = ['pizza', 'samosa', 'pasta', 'biryani', 'burger']
+    def front(self):
+        return self.buffer[-1]
 
-    pq = Queue()
+def produce_binary_numbers(n):
+    numbers_queue = Queue()
+    numbers_queue.enqueue("1")
 
-    t1 = threading.Thread(target=pq.enqueue, args=(orders,))
-    t2 = threading.Thread(target=pq.dequeue)
+    for i in range(n):
+        front = numbers_queue.front()
+        print("   ", front)
+        numbers_queue.enqueue(front + "0")
+        numbers_queue.enqueue(front + "1")
+        print(numbers_queue.buffer)
 
-    t1.start()
-    t2.start()
+        numbers_queue.dequeue()
+        print(numbers_queue.buffer)
 
-    t1.join()
-    t2.join()
+
+if __name__ == '__main__':
+    produce_binary_numbers(10)
