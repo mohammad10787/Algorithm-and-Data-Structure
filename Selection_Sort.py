d048408c7659c9f89fabfb217eb3c9e1a50c7f76
Mohammad Afshari <mohammad.afshari2@mail.mcgill.ca>
2024-04-29 16:46:49 -0700

    Update Selection_Sort.py

--- Selection_Sort.py
+++ Selection_Sort.py
@@ -1,24 +1,38 @@
 # Selection sort of order O(N^2)
-def selection_sort(arr):
+def selection_sort(arr, list):
     size = len(arr)
-    for i in range(size-1):
-        min_index = i
-        for j in range(min_index+1,size):
-            if arr[j] < arr[min_index]:
-                min_index = j
-        if i != min_index:
-            arr[i], arr[min_index] = arr[min_index], arr[i]
+    size_list = len(list)
+    for k in range (size_list):
+        for i in range(size-1):
+            min_index = i
+            for j in range(min_index+1,size):
+                if arr[j][list[k]] < arr[min_index][list[k]]:
+                    min_index = j
+            if i != min_index:
+                arr[i], arr[min_index] = arr[min_index], arr[i]
 
 
 if __name__ == '__main__':
-    tests = [
-        [89, 78, 61, 53, 23, 21, 17, 12, 9, 7, 6, 2, 1],
-        [],
-        [1,5,8,9],
-        [234,3,1,56,34,12,9,12,1300],
-        [78, 12, 15, 8, 61, 53, 23, 27],
-        [5]
+    elements = [
+        {'First Name': 'Raj', 'Last Name': 'Nayyar'},
+        {'First Name': 'Suraj', 'Last Name': 'Sharma'},
+        {'First Name': 'Karan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jade', 'Last Name': 'Canary'},
+        {'First Name': 'Raj', 'Last Name': 'Thakur'},
+        {'First Name': 'Raj', 'Last Name': 'Sharma'},
+        {'First Name': 'Kiran', 'Last Name': 'Kamla'},
+        {'First Name': 'Armaan', 'Last Name': 'Kumar'},
+        {'First Name': 'Jaya', 'Last Name': 'Sharma'},
+        {'First Name': 'Ingrid', 'Last Name': 'Galore'},
+        {'First Name': 'Jaya', 'Last Name': 'Seth'},
+        {'First Name': 'Armaan', 'Last Name': 'Dadra'},
+        {'First Name': 'Ingrid', 'Last Name': 'Maverick'},
+        {'First Name': 'Aahana', 'Last Name': 'Arora'}
     ]
-    for elements in tests:
-        selection_sort(elements)
-        print(elements)
+
+    print('Given unsorted array:', *elements, sep='\n')
+
+    list = ['First Name','Last Name']
+    selection_sort(elements, list)
+
+    print('Sorted array:', *elements, sep='\n')
